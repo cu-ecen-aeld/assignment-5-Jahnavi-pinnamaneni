699a2f1373d50c7966eecdd0a5fc994dd71d52a9
Jahnavi-pinnamaneni <dolly.pinnamaneni@gmail.com>
2022-02-20 01:34:08 -0700

    Passed full test

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 496418687d974dcf18823fde924345634f048a30
+AESD_ASSIGNMENTS_VERSION = 'be6b995e61cad2bfebfd25835219cdbc86060a71' 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Jahnavi-pinnamaneni.git
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Jahnavi-pinnamaneni.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
