98b7264721de92a99664467a3ad099e9fd5796d1
Jahnavi-pinnamaneni <dolly.pinnamaneni@gmail.com>
2022-03-14 17:28:12 -0600

    Fulltest of A8 passed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'a29c1d6eec66decf6e041a0701659ada29306779' 
+AESD_ASSIGNMENTS_VERSION = '9a7e78f9429de271f0a6b374d135b778d922d4b2' 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,9 +14,12 @@ AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Jah
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS += aesd-char-driver/
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/aesd-char-driver
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -30,6 +33,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/dependencies.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/usr/bin/
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
--- base_external/rootfs-overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs-overlay/etc/init.d/S98lddmodules.sh
@@ -6,12 +6,14 @@ case "$1" in
         echo "Starting init script for Module Loading"
         /usr/bin/scull_load
         /usr/bin/module_load faulty
+        /usr/bin/aesdchar_load
         modprobe hello
         ;;
     stop)
         echo "Removing user modules"
         /usr/bin/scull_unload
         /usr/bin/module_unload faulty
+        /usr/bin/aesdchar_unload
         rmmod hello
         ;;
     *)
